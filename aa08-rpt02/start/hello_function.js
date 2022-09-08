923fb955fde77f43d0fed7193e4752f12fe83765
TAaHwa <100752582+TAaHwa@users.noreply.github.com>
2022-09-08 16:37:45 +0900

    Add files via upload

---
+++ aa08-rpt02/start/hello_function.js
@@ -0,0 +1,7 @@
+//hello_function.js
+function hello(what){
+    console.log("Hello " + what + " !");
+}
+
+hello("aa00");
+hello("redwoods, 김 태 화");
---
+++ aa08-rpt02/start/index_aa08.js
@@ -0,0 +1,5 @@
+var myinfo = require('./aa08info');
+
+myinfo("aa08","Taahwa","010-4547-2937");
+
+myinfo("aa66","Jew","010-4547-2929");
